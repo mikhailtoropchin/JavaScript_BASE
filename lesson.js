d0072f732483158663fa39b697e1e545efa20cc8
mikhail <toropchin.epid@yandex.ru>
2022-03-26 12:38:49 +0700

    done

---
+++ lesson.js
@@ -0,0 +1,18 @@
+//var x = 2;
+//console.log(x);
+
+// function declaration
+function f (a, b) {
+    return a + b;
+}
+
+
+// function expression
+var f_2 = function (a, b) {
+    return a + b;
+}
+
+// arrow function
+var f_3 = (a, b) => {
+    return a + b;
+}
